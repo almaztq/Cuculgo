4ea56eafd5e9f06f8dbea7037115dc47e9c7b65d
almaztq <116556943+almaztq@users.noreply.github.com>
2022-10-24 17:51:13 +0700

    adding and fixing feature
    adding echo and parsing json features 8 lines

--- cegol.php
+++ cegol.php
@@ -1,6 +1,13 @@
 <?php
 echo ("This program created by SyukaAlmat");
+echo ("for joining the hacktoberfest2022");
+echo ("=================================");
+echo ("for joining the hacktoberfest2022");
+echo ("           SYUKA TEAM            ");
+echo ("              2022               ");
+echo ("  WELCOME FOR hacktoberfest2022  ");
 curl https://www.google.com
 file_get_contents = url("https://www.gooogle.com");
-
+$url = json("./load.txt");
+echo ($url);
 ?>
